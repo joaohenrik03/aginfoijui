ab2fe8f860e34d90c4fc9711022d469aeab22d0c
João Henrik Signori Zilch <84729916+joaohenrik03@users.noreply.github.com>
2022-03-17 19:27:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -48,3 +48,22 @@ const swiper = new Swiper('.swiper-container', {
       }
     }
 })
+
+/* ScrollReveal */
+
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+
+}) 
+
+scrollReveal.reveal(`
+    #home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links`, 
+    { interval: 100 }
+)
