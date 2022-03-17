c726f037a351f37fe0dc8a55fc2815a28eaaf351
João Henrik Signori Zilch <84729916+joaohenrik03@users.noreply.github.com>
2022-03-17 13:45:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -18,3 +18,16 @@ for (const links of linksMenu) {
         nav.classList.remove('show')   
     })
 }
+
+/* Colocar sombra ao header quando der scroll */ 
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY >= navHeight) {
+        header.classList.add('scroll')       
+    } else {
+        header.classList.remove('scroll')       
+    }
+})
