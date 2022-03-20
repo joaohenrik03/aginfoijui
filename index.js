da9a1ad52880656814d26ffd55bd1541332ea7d9
João Henrik Signori Zilch <84729916+joaohenrik03@users.noreply.github.com>
2022-03-20 01:12:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -24,31 +24,29 @@ for (const links of linksMenu) {
 /* Colocar sombra ao header quando der scroll */ 
 /* Put a shadow on the header when scrolling */
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', () => {
+function headerShadow() {
+    const header = document.querySelector('#header')
+    const navHeight = header.offsetHeight    
     if (window.scrollY >= navHeight) {
         header.classList.add('scroll')       
     } else {
         header.classList.remove('scroll')       
     }
-})
+}
 
 /* Adicionar botão de voltar ao topo quando passar da seção home */
 /* Add back to top button when past home section */
 
-const buttonBackToTop = document.querySelector('.back-to-top')
-const home = document.querySelector('#home')
-const homeHeight = home.offsetHeight
-
-window.addEventListener('scroll', () => {
+function backToTop() {
+    const buttonBackToTop = document.querySelector('.back-to-top')
+    const home = document.querySelector('#home')
+    const homeHeight = home.offsetHeight
     if (window.scrollY > homeHeight) {
         buttonBackToTop.classList.add('back-to-top-show')  
     } else if (window.scrollY < homeHeight) {
         buttonBackToTop.classList.remove('back-to-top-show')      
     }
-})
+}
 
 /* Carrossel de depoimentos */
 /* Testimonial carousel */
@@ -88,3 +86,10 @@ scrollReveal.reveal(`
     footer .brand, footer .social`, 
     { interval: 100 }
 )
+
+/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
+
+window.addEventListener('scroll', () => {
+    headerShadow()
+    backToTop()
+})
