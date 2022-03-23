b421d1e084628df0ff3348f27180d1e308f47797
João Henrik Signori Zilch <84729916+joaohenrik03@users.noreply.github.com>
2022-03-22 22:21:02 -0300

    Update index.js and style.css

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ for (const links of linksMenu) {
 /* Colocar sombra ao header quando der scroll */ 
 /* Put a shadow on the header when scrolling */
 
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight    
 function headerShadow() {
-    const header = document.querySelector('#header')
-    const navHeight = header.offsetHeight    
     if (window.scrollY >= navHeight) {
         header.classList.add('scroll')       
     } else {
@@ -37,10 +37,10 @@ function headerShadow() {
 /* Adicionar botão de voltar ao topo quando passar da seção home */
 /* Add back to top button when past home section */
 
+const buttonBackToTop = document.querySelector('.back-to-top')
+const home = document.querySelector('#home')
+const homeHeight = home.offsetHeight
 function backToTop() {
-    const buttonBackToTop = document.querySelector('.back-to-top')
-    const home = document.querySelector('#home')
-    const homeHeight = home.offsetHeight
     if (window.scrollY > homeHeight) {
         buttonBackToTop.classList.add('back-to-top-show')  
     } else if (window.scrollY < homeHeight) {
@@ -87,9 +87,35 @@ scrollReveal.reveal(`
     { interval: 100 }
 )
 
+/* Menu ativo no nav */
+/* Active menu without navigation */
+
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+    const checkPoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for (const section of sections) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+
+        const checkPointStart = checkPoint >= sectionTop
+        const checkPointEnd = checkPoint <= sectionTop + sectionHeight
+
+        if (checkPointStart && checkPointEnd) {
+            document.querySelector('nav ul li a[href*=' + sectionId + ']' )
+            .classList.add('active')
+        } else {
+            document.querySelector('nav ul li a[href*=' + sectionId + ']' )
+            .classList.remove('active')
+        }
+    }
+}
+
 /* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
 
 window.addEventListener('scroll', () => {
     headerShadow()
     backToTop()
+    activateMenuAtCurrentSection()
 })
